94f6cfc3f1f13c6372dbc29f49bde913d48409f8
Taranpreet Singh <sin14121@sheridancollege.ca>
2024-02-20 23:15:26 -0500

    dfghj

---
+++ src/mid_practice/Mid_practice.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mid_practice;
+
+/**
+ *
+ * @author Taranpreet Singh
+ */
+public class Mid_practice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/mid_practice/Syllabus.java
@@ -0,0 +1,36 @@
+package mid_practice;
+
+public class Syllabus {
+
+	private int Chapters;
+	private int Topics;
+
+	public void getChapters() {
+		// TODO - implement Syllabus.getChapters
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Chapters
+	 */
+	public void setChapters(int Chapters) {
+		// TODO - implement Syllabus.setChapters
+		throw new UnsupportedOperationException();
+	}
+
+	public void getTopics() {
+		// TODO - implement Syllabus.getTopics
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Topics
+	 */
+	public void setTopics(int Topics) {
+		// TODO - implement Syllabus.setTopics
+		throw new UnsupportedOperationException();
+	}
+
+}
